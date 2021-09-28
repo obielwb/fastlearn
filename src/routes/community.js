eb770e9f26803630e2fe6578a8db060b54f7b9df
Gabriel Willian <obielwb@gmail.com>
2021-09-28 11:53:03 -0300

    added community session

---
+++ src/routes/community.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const communityRouter = express.Router();
+
+communityRouter.get('/community', async (req, res) => {
+    res.render("community");
+});
+
+module.exports = communityRouter;
