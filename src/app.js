dd36f41679d5ba329554100a0acdbb5493a691c5
oJPBarbosa <contato.ferb@gmail.com>
2021-10-07 10:43:48 -0300

    separete app from server

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', './src/views');
+
+app.use(express.static('public'));
+app.use('/icons', express.static('./public/icons'));
+app.use('/images', express.static('./public/images'));
+app.use('/scripts', express.static('./public/scripts'));
+app.use('/css', express.static('./public/styles/css'));
+
+const homeRouter = require('./routes/home');
+const communityRouter = require('./routes/community');
+const postRouter = require('./routes/post');
+const userRouter = require('./routes/user');
+
+app.use('/', homeRouter);
+app.use('/community', communityRouter);
+app.use('/post', postRouter);
+app.use('/user', userRouter);
+
+module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,24 +1,4 @@
-const express = require('express');
-const app = express();
-
-app.set('view engine', 'ejs');
-app.set('views', './src/views');
-
-app.use(express.static('public'));
-app.use('/icons', express.static('./public/icons'));
-app.use('/images', express.static('./public/images'));
-app.use('/scripts', express.static('./public/scripts'));
-app.use('/css', express.static('./public/styles/css'));
-
-const homeRouter = require('./routes/home');
-const communityRouter = require('./routes/community');
-const postRouter = require('./routes/post');
-const userRouter = require('./routes/user');
-
-app.use('/', homeRouter);
-app.use('/community', communityRouter);
-app.use('/post', postRouter);
-app.use('/user', userRouter);
-
+const app = require('./app');
 const port = process.env.PORT || 3000;
+
 app.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
