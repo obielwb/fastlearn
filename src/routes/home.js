ed34703973ccc1caa995a2c7ee78778d4a488296
João Pedro Barbosa <contato.ferb@gmail.com>
2021-10-11 21:18:16 -0300

    update home.js

--- src/routes/home.js
+++ src/routes/home.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 
-
+// Using Newscatcher API for DEMO purposes - https://newscatcherapi.com/
 router.get('', async (req, res) => {
   const options = {
     method: 'GET',
