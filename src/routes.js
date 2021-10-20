c0f5dfdf47435f26a25dd1dbf568ad7842e43b51
João Pedro Barbosa <contato.ferb@gmail.com>
2021-10-20 20:41:11 -0300

    update routes.js

--- src/routes.js
+++ src/routes.js
@@ -7,12 +7,13 @@ const routes = express.Router();
 
 routes.get('/users', UserController.index);
 routes.get('/users/:username', UserController.get);
+routes.post('/users', UserController.store);
+
 routes.get('/posts', PostController.index);
 routes.get('/posts/:id', PostController.get);
+routes.post('/posts', PostController.store);
+
 routes.get('/community', CommunityController.index);
 routes.get('/community/:name', CommunityController.get);
-
-routes.post('/users', UserController.store);
-routes.post('/posts', PostController.store);
 
 module.exports = routes;
