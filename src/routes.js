13ee68f60f79435948cc174a931fc98efaad5232
João Pedro Barbosa <contato.ferb@gmail.com>
2021-10-21 18:31:31 -0300

    update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const UserController = require('./controllers/UserController');
 const PostController = require('./controllers/PostController');
 const CommunityController = require('./controllers/CommunityController');
