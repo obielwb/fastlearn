a40cfb263ae5ed4fb37e41f08be564e1a53bb085
oJPBarbosa <contato.ferb@gmail.com>
2021-10-24 15:53:44 -0300

    add missing routes to routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,32 @@
 const express = require('express');
 
-const UserController = require('./controllers/UserController');
-const PostController = require('./controllers/PostController');
 const CommunityController = require('./controllers/CommunityController');
+const PathController = require('./controllers/PathController');
+const PostController = require('./controllers/PostController');
+const SubscriptionController = require('./controllers/SubscriptionController');
+const UserController = require('./controllers/UserController');
 
 const routes = express.Router();
 
-routes.get('/users', UserController.index);
-routes.get('/users/:username', UserController.show);
-routes.post('/users', UserController.store);
+routes.get('/communities', CommunityController.index);
+routes.get('/communities/:name', CommunityController.show);
+routes.post('/communities/:name', CommunityController.store);
+
+routes.get('/paths', PathController.index);
+routes.get('/paths/:path_id', PathController.show);
+routes.post('/paths', PathController.store);
 
 routes.get('/posts', PostController.index);
 routes.get('/posts/:id', PostController.show);
 routes.post('/posts', PostController.store);
 
-routes.get('/community', CommunityController.index);
-routes.get('/community/:name', CommunityController.show);
+// TODO: make subscriptions route private
+routes.get('/users/subscriptions', SubscriptionController.index);
+routes.get('/users/:user_id/subscriptions', SubscriptionController.show);
+routes.post('/users/:user_id/subscriptions', SubscriptionController.store);
+
+routes.get('/users', UserController.index);
+routes.get('/users/:id', UserController.show);
+routes.post('/users', UserController.store);
 
 module.exports = routes;
