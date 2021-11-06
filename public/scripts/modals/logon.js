9b6a3376aaf6681058af171698df60a0619ccfa4
oJPBarbosa <contato.ferb@gmail.com>
2021-11-05 22:42:34 -0300

    update logon modal

--- public/scripts/modals/logon.js
+++ public/scripts/modals/logon.js
@@ -1,16 +1,16 @@
+
 const modal = document.querySelector('#logon-modal');
-const buttons = document.querySelectorAll('.logon-button');
+const button = document.querySelector('#logon-button');
 const close = document.querySelector('#logon-close-button');
+
 
 modal.addEventListener('click', (event) => {
   if (event.target.id == 'logon-modal')
     modal.classList.remove('show');
 });
 
-buttons.forEach(button => {
-  button.addEventListener('click', () => {
-    modal.classList.add('show');
-  });
+button.addEventListener('click', () => {
+  modal.classList.add('show');
 });
 
 close.addEventListener('click', () => {
--- public/scripts/theme.js
+++ public/scripts/theme.js
@@ -1,5 +1,8 @@
 const root = getComputedStyle(document.body);
 const theme = root.getPropertyValue('--root-theme').substring(2, root.getPropertyValue('--root-theme').length - 1);
-const logo = document.querySelector('#logo');
 
-logo.setAttribute('src', `/images/logos/${theme}.png`);
+const logos = document.querySelectorAll('.logo');
+
+logos.forEach(logo => {
+  logo.setAttribute('src', `/images/logos/${theme}.png`);
+});
