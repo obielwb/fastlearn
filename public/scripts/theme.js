23ce30c9d41961f4c0ff2b25667aa64436bd17ae
oJPBarbosa <contato.ferb@gmail.com>
2021-11-08 18:42:11 -0300

    add final themes modal

--- public/scripts/theme.js
+++ public/scripts/theme.js
@@ -1,8 +1,48 @@
-const root = getComputedStyle(document.body);
-const theme = root.getPropertyValue('--root-theme').substring(2, root.getPropertyValue('--root-theme').length - 1);
-
 const logos = document.querySelectorAll('.logo');
 
-logos.forEach(logo => {
-  logo.setAttribute('src', `/images/logos/${theme}.png`);
+const solar = document.querySelector('#solar-theme');
+const moon = document.querySelector('#moon-theme');
+const rocket = document.querySelector('#rocket-theme');
+
+const themes = [solar, moon, rocket];
+
+const reference = {
+  'solar': solar,
+  'moon': moon,
+  'rocket': rocket,
+};
+
+const changeLogo = (theme) => {
+  logos.forEach(logo => {
+    logo.setAttribute('src', `/images/logos/${theme}.png`);
+  });
+};
+
+const verifyTheme = () => {
+  if (localStorage.getItem('theme') == null)
+    localStorage.setItem('theme', 'solar');
+
+  const currentTheme = localStorage.getItem('theme');
+  reference[currentTheme].checked = true;
+
+  document.querySelector(':root').classList.toggle(currentTheme);
+
+  changeLogo(currentTheme);
+};
+
+themes.forEach(theme => {
+  theme.addEventListener('change', (event) => {
+    document.querySelector(':root').classList.toggle(localStorage.getItem('theme'));
+
+    const value = event.target.value;
+    localStorage.setItem('theme', value);
+
+    reference[value].checked = true;
+
+    document.querySelector(':root').classList.toggle(value);
+
+    changeLogo(value);
+  });
 });
+
+window.addEventListener('load', verifyTheme);
--- src/routes/home.js
+++ src/routes/home.js
@@ -1,31 +1,29 @@
+require('dotenv').config();
+
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 
 // Using Newscatcher API for DEMO purposes - https://newscatcherapi.com/
 router.get('', async (req, res) => {
-  const options = {
-    method: 'GET',
-    url: 'https://api.newscatcherapi.com/v2/search',
-    params: {
-      q: 'unicamp',
-      lang: 'en',
-      sort_by: 'relevancy',
-      page: '1'
-    },
-    headers: {
-      'x-api-key': process.env.NEWSCATCHER_API_KEY
-    }
-  }
-
   let user = {};
   let posts = [];
 
   try {
-    const response = (await axios.request(options)).data;
+    const { data, status } = await axios.get('https://api.newscatcherapi.com/v2/search', {
+      params: {
+        q: 'unicamp',
+        lang: 'en',
+        sort_by: 'relevancy',
+        page: '1'
+      },
+      headers: {
+        'x-api-key': `'${process.env.NEWSCATCHER_API_KEY}'`
+      },
+    });
 
-    if (response.status == 'ok') {
-      response.articles.forEach(article => {
+    if (status == 200) {
+      data.articles.forEach(article => {
         const post = {
           author: article.author,
           comments: parseInt(article._score),
@@ -44,7 +42,9 @@ router.get('', async (req, res) => {
 
     else
       posts = null;
-  } catch (error) {}
+  } catch (error) {
+    console.log(error);
+  }
 
   // try {
   //   const response = (await axios.get(`https://fastlearn-api.herokuapp.com/home/user/${user.name}`)).data;
