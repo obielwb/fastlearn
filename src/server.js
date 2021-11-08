5ea0de565272a17ae5891ab315c95c2f42f37a16
oJPBarbosa <contato.ferb@gmail.com>
2021-11-08 13:06:14 -0300

    address include reference issue

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
 const app = require('./app');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3030;
 
 app.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
