93ef110dd088799fd1619570b149a371d03bae5f
oJPBarbosa <contato.ferb@gmail.com>
2021-11-19 17:22:57 -0300

    update post

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,12 @@
-const express = require('express');
+const express = require("express");
 const routes = express.Router();
 
-const home = require('./home');
-const community = require('./community');
-const post = require('./post');
-const user = require('./user');
+const home = require("./home");
+const community = require("./community");
+const post = require("./post");
 
-routes.use('/', home);
-routes.use('/community', community);
-routes.use('/post', post);
-routes.use('/user', user);
+routes.use("/", home);
+routes.use("/community", community);
+routes.use("/post", post);
 
 module.exports = routes;
